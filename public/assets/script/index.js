95121144f005d4794148ef467542f5f28a11cd77
jflsk <jfs.trc@gmail.com>
2024-02-07 14:57:57 -0300

    fix: syntax error

--- public/assets/script/index.js
+++ public/assets/script/index.js
@@ -1,6 +1,6 @@
 const html = document.querySelector('html')
 const focoBt = document.querySelector('.app__card-button--foco')
-const curtoBt = document.querySelector('app__card-button--curto')
+const curtoBt = document.querySelector('.app__card-button--curto')
 const longoBt = document.querySelector('.app__card-button--longo')
 
 focoBt.addEventListener('click', () => {
